13993462af54c29b60902d61cd552ddcb159f360
PablorellanaM <ore20555@uvg.edu.gt>
2024-07-18 12:08:56 -0600

    Funcion para convertir de decimal a Binario

--- Problema_1.kt
+++ Problema_1.kt
@@ -1,3 +1,17 @@
 fun main() {
     println("Ingrese un numero entero:")
-    val decimal = readLine()?.toIntOrNull()}
+    val decimal = readLine()?.toIntOrNull()
+
+
+    if (decimal == null) {
+        println("Por favor, ingrese un número válido.")
+        return
+    }
+
+    val binario = convertDecimalToBinary(decimal)
+    println("El numero $decimal convertido a binario es: $binario")
+}
+
+fun convertDecimalToBinary(decimal: Int): String {
+    return Integer.toBinaryString(decimal)
+}
