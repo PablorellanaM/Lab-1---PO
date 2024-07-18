d51e8f486a6dc31d5e138d065770aa3cdd41fa54
PablorellanaM <ore20555@uvg.edu.gt>
2024-07-18 12:31:05 -0600

    If es Null o Vacio

--- Problema_2.kt
+++ Problema_2.kt
@@ -1,3 +1,10 @@
 fun main() {
     println("Ingrese varias palabras separadas por espacios:")
-    val input = readLine()?.trim() }
+    val input = readLine()?.trim()
+
+    if (input.isNullOrEmpty()) {
+        println("Por favor, ingrese palabras válidas.")
+        return
+    }
+
+}
